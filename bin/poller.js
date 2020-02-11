07deb8736968f5486a65f4645559f94aa6263fa1
Andy Armstrong <andy@hexten.net>
2020-02-11 20:19:44 +0000

    Don't write to FS.

--- bin/poller.js
+++ bin/poller.js
@@ -6,27 +6,15 @@ const config = require("config");
 const { SmartHub } = require("ee/smarthub");
 const DB = require("db/smarthub");
 const Promise = require("bluebird");
-const mkdirp = Promise.promisify(require("mkdirp"));
-const path = require("path");
-const writeFileAtomic = Promise.promisify(require("write-file-atomic"));
 const nano = require("nano");
 const moment = require("moment");
-const _ = require("lodash");
 const { BehaviorSubject } = require("rxjs");
 const { mergeMap, multicast, refCount, filter } = require("rxjs/operators");
 const { cron } = require("rxx");
 
-async function saveJSON(file, data) {
-  console.log(`Saving ${file}`);
-  const dir = path.dirname(file);
-  await mkdirp(dir);
-  await writeFileAtomic(file, JSON.stringify(data, null, 2));
-}
-
 try {
   const db = new DB(nano(config.db.url));
   const sh = new SmartHub(config.router);
-  const outDir = path.join(config.state, "network");
 
   const feed$ = cron(config.interval).pipe(
     mergeMap(now => Promise.props({ now, network: sh.getMyNetwork() })),
@@ -38,15 +26,6 @@ try {
   feed$
     .pipe(
       mergeMap(({ now, network }) => db.stashSample(moment.utc(now), network))
-    )
-    .subscribe();
-
-  feed$
-    .pipe(
-      mergeMap(({ now, network }) => {
-        const name = moment.utc(now).format() + ".json";
-        return saveJSON(path.join(outDir, name), network);
-      })
     )
     .subscribe();
 } catch (e) {
