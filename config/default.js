68671945783e02233571fb766a70e4afc6619b34
Andy Armstrong <andy@hexten.net>
2022-03-01 18:09:13 +0000

    Remove unused.

--- config/default.js
+++ config/default.js
@@ -1,9 +1,5 @@
 "use strict";
 
 module.exports = {
-  db: {
-    url: "http://chaise:sofa@localhost:5984/smarthub"
-  },
-  router: "http://192.168.1.1",
-  interval: 60000
+  router: "http://192.168.1.1"
 };
